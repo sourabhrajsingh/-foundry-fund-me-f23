6db60fcd1f9912cebba82c581c283bf5a0135759
sourabh yadav <sourabh.works@gmail.com>
2024-09-12 12:15:20 +0530

    Add Script and Test
    - Add DeployFundMe and HelperConfig script
    - Add FundMeTest
    - Set up MockV3Aggregator

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../src/fundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployFundMe is Script {
+
+    function run() external returns(FundMe fundMe) {
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPrice = helperConfig.activeNetworkConfig();
+        vm.startBroadcast();
+        fundMe = new FundMe(ethUsdPrice);
+        vm.stopBroadcast();
+    }
+}
---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/fundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+
+
+contract FundMeTest is Test {
+    
+    FundMe fundMe;
+    DeployFundMe deployFundMe;
+
+    function setUp() external {
+        deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        assertEq(fundMe.getVersion(), 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert();
+
+        fundMe.fund();
+    }
+
+}
